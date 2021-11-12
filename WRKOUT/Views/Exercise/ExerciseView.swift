ca9ce7283f365576cabd49bf04265ae101d52501
Alexander Voigt <alexander.voigt@novatec-gmbh.de>
2021-11-12 15:20:53 +0100

    WRKOUT-5: Fix grouping by date

--- WRKOUT/Views/Exercise/ExerciseView.swift
+++ WRKOUT/Views/Exercise/ExerciseView.swift
@@ -16,70 +16,83 @@ struct ExerciseView: View {
     var title: String
     
     @Environment(\.managedObjectContext) private var viewContext
-    @FetchRequest(entity: Set.entity(), sortDescriptors: [], predicate: nil)
+    @FetchRequest(entity: Set.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Set.created, ascending: false)], predicate: nil)
     var sets: FetchedResults<Set>
     
     let formatter: NumberFormatter = {
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal
-            return formatter
-        }()
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
     
     var body: some View {
         List {
-            ForEach(sets.filter {
-                $0.exercise == exercise
-            }) { (exerciseSet) in
+            ForEach(groupList(sets: sets)) { (exerciseSet: Set) in
                 Section(header: Text(formatDate(date: exerciseSet.created!))) {
-                    ForEach(sets.filter {
-                        $0.created == exerciseSet.created
-                    }) { row in
+                    ForEach(sets) { row in
                         ExerciseRow(reps: Int(row.reps), weight: row.weight).font(.body)
                     }
                 }
             }
         }
-            // New set sheet
-            .sheet(isPresented: $showSheet) {
-                NavigationView {
-                    VStack {
-                        Divider()
-
-                        Text("Reps").font(Font.body.weight(.bold))
-                        Stepper("\(repCount)",
-                                value: $repCount,
-                                in: 0...100)
-                        Divider()
-
-                        Text("Weight").font(Font.body.weight(.bold))
-                        TextField("Weight", value: $weightCount, formatter: formatter)
-                            .font(Font.body.weight(.medium))
-                            .padding(5)
-                            .keyboardType(.decimalPad)
-                        Button(action: {
-                            saveNewSet(reps: $repCount.wrappedValue, weight: $weightCount.wrappedValue)
-                            self.showSheet = false
-                        }) {
-                            Text("Add")
-                        }.disabled(weightCount == 0.0 || repCount == 0)
+        
+        //        List {
+        //            ForEach(sets.filter {
+        //                // get all sets for given exercise
+        //                $0.exercise == exercise
+        //            }) { (exerciseSet: Set) in
+        //                Section(header: Text(formatDate(date: exerciseSet.created!))) {
+        //                    ForEach(sets.filter {
+        //                        Calendar.current.compare($0.created!, to: exerciseSet.created!, toGranularity: Calendar.Component.day) == .orderedDescending
+        //                        // checkIfSameDay(date1: $0.created!, date2: exerciseSet.created!)
+        //                    }) { row in
+        //                        ExerciseRow(reps: Int(row.reps), weight: row.weight).font(.body)
+        //                    }
+        //                }
+        //            }
+        //        }
+        
+        // New set sheet
+        .sheet(isPresented: $showSheet) {
+            NavigationView {
+                VStack {
+                    Divider()
+                    
+                    Text("Reps").font(Font.body.weight(.bold))
+                    Stepper("\(repCount)",
+                            value: $repCount,
+                            in: 0...100)
+                    Divider()
+                    
+                    Text("Weight").font(Font.body.weight(.bold))
+                    TextField("Weight", value: $weightCount, formatter: formatter)
+                        .font(Font.body.weight(.medium))
+                        .padding(5)
+                        .keyboardType(.decimalPad)
+                    Button(action: {
+                        saveNewSet(reps: $repCount.wrappedValue, weight: $weightCount.wrappedValue)
+                        self.showSheet = false
+                    }) {
+                        Text("Add")
+                    }.disabled(weightCount == 0.0 || repCount == 0)
                         .buttonStyle(CustomButtonStyle())
-                    }
-                    .toolbar {
-                        ToolbarItem(placement: .primaryAction) {
-                            Button(action: {
-                                self.showSheet = false
-                                cleanFields()
-                            }) {
-                                Text("Done").fontWeight(.semibold)
-                            }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button(action: {
+                            self.showSheet = false
+                            cleanFields()
+                        }) {
+                            Text("Done").fontWeight(.semibold)
                         }
                     }
                 }
             }
-
+        }
+        
         .navigationBarTitle(Text(exercise.name!), displayMode: .inline)
         .toolbar {
-
+            
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button(action: {
                     self.showSheet = true
@@ -116,15 +129,45 @@ struct ExerciseView: View {
         weightCount = 0.0
     }
     
+    private func groupList(sets: FetchedResults<Set>) -> FetchedResults<Set> {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        var dates: [Date] = []
+        
+        sets.forEach { singleSet in
+            dateFormatter.date(from: singleSet.created!.description)
+            dates.append(Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: singleSet.created!))!)
+        }
+        print("dates", dates)
+        let groupedSets = Dictionary(grouping: sets, by: { $0.created })
+        
+        
+        return sets
+    }
+    
     /**
      Formats a given date to a useful format
      */
     private func formatDate(date: Date) -> String {
+        print("format date", date)
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
         
         return dateFormatter.string(from: date)
+    }
+    
+    private func checkIfSameDay(date1: Date, date2: Date) -> Bool {
+        let cal = Calendar.current
+        return cal.isDate(date1, inSameDayAs: date2)
+    }
+    
+    private func sortDates(date1: Date, date2: Date) -> Bool {
+        let cal = Calendar.current
+        let res = cal.compare(date1, to: date2, toGranularity: Calendar.Component.day)
+        return true
     }
 }
 
@@ -139,7 +182,7 @@ public struct CustomButtonStyle: ButtonStyle {
             .background(
                 RoundedRectangle(cornerRadius: 14.0, style: .continuous)
                     .fill(Color.accentColor)
-                )
+            )
             .opacity(configuration.isPressed ? 0.4 : 1.0)
     }
 }
