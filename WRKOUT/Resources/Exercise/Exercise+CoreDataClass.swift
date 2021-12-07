226b66f22fcfa0373872a0146b9b6d91c50868d3
Alexander Voigt <aesydoesit@googlemail.com>
2021-12-07 19:40:54 +0100

    Add App icon

---
+++ WRKOUT/Resources/Exercise/Exercise+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Exercise+CoreDataClass.swift
+//  WRKOUT
+//
+//  Created by Alex Privado on 07.12.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Exercise)
+public class Exercise: NSManagedObject {
+
+}
